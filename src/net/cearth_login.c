551831b5c8a36d14e5b967cc5c0b530dc93727e7
nyanpasu <nyanpasugit@gmail.com>
2014-06-24 04:10:46 +0800

    Write more function prototypes that don't work
    Needs to be corrected in 0.1.1

--- src/net/cearth_login.c
+++ src/net/cearth_login.c
@@ -1,15 +1,13 @@
 #include "cearth_login.h"
 
-int
-logindb_open(struct cearth_logindb *db)
+cearth_logindb*
+logindb_open(void)
 {
-        db->n = 0;
-        memset(db->name, 0, 100);
-        memset(db->cookie, 0, 100);
+        cearth_logindb *db = calloc(sizeof(cearth_logindb), 1);
 
         FILE *fp = fopen(LOGINDB_FILE, "r+");
         if (fp == NULL)
-                return -1;
+                return NULL;
 
         char line[LOGINDB_MAXLINE];
         fgets(line, LOGINDB_MAXLINE, fp);
@@ -23,17 +21,19 @@ logindb_open(struct cearth_logindb *db)
 
                        char *cookie = strchr(line, ';') + 1;
                        int namelen = cookie - line - 1;
-                       int cookielen = strlen(cookie) - 1;
+                       int cookielen = strlen(cookie); 
+                       if (cookie[cookielen] == '\n') {
+                               cookie[cookielen] = 0;
+                       }
 
                        /* +1 for the NULL char. */
-                       char *name      = (char*)malloc(namelen + 1);
-                       char *cookienew = (char*)malloc(cookielen + 1);
-                       memset(name, '\0', namelen + 1);
-                       memset(cookienew, '\0', cookielen + 1);
+                       char *name      = calloc(namelen + 1, sizeof(char));
+                       char *cookienew = calloc(LOGIN_COOKIESIZE, sizeof(char));
+                       /* TODO Use strtok */
                        memcpy(name, line, namelen);
                        memcpy(cookienew, cookie, cookielen);
                        
-                       db->name[db->n] = name;
+                       db->name  [db->n] = name;
                        db->cookie[db->n] = cookienew;
                        db->n++;
                }
@@ -41,17 +41,17 @@ logindb_open(struct cearth_logindb *db)
         }
 
         fclose(fp);
-        return 1;
+        return db;
 }
 
-int
-logindb_close(struct cearth_logindb *db)
+void
+logindb_close(cearth_logindb *db)
 {
         FILE *fp = fopen(LOGINDB_FILE, "w+");
 
         if (fp == NULL) {
-                printf("Couldn't open LOGINDB_FILE\n");
-                return 0;
+                perror("Couldn't open LOGINDB_FILE\n");
+                return;
         }
 
         for (int i = 0; i < db->n; ++i)
@@ -60,19 +60,21 @@ logindb_close(struct cearth_logindb *db)
                 fputc(';', fp);
                 fputs(db->cookie[i], fp);
                 fputc('\n', fp);
+
+                free(db->name[i]);
+                free(db->cookie[i]);
         }
 
-        return 1;
+        free(db);
 }
 
 int
-logindb_useradd(struct cearth_logindb *db, char *user)
+logindb_useradd(cearth_logindb *db, const char *user)
 {
-        char *newname = (char *)malloc(strlen(user)+1);
-        char* newcookie = (char *)malloc(1);
+        char *newname   = calloc(strlen(user)+1, 1);
+        char *newcookie = calloc(LOGIN_COOKIESIZE, 1);
 
         strcpy(newname, user);
-        *newcookie = '\0';
 
         db->n++;
         db->name[db->n-1] = newname;
@@ -82,7 +84,7 @@ logindb_useradd(struct cearth_logindb *db, char *user)
 }
 
 int
-logindb_usercheck(struct cearth_logindb *db, char *user)
+logindb_usercheck(cearth_logindb *db, const char *user)
 {
         if (db->n == 0)
                 return 0;
@@ -101,22 +103,66 @@ logindb_usercheck(struct cearth_logindb *db, char *user)
         return 0;
 }
 
-void
-loginhttp_gettoken(const char *user, const char *cookie)
+int
+logindb_userget(cearth_logindb *db, const char *user)
 {
-        printf("Connecting to %s to get token for user '%s'...\n", haven_webauth, user);
-        CURL *handle = curl_easy_init();
+        if (db->n == 0)
+                return -1;
+
+        for (int i = 0; i < db->n; ++i)
+        {
+                if (strcmp(db->name[i], user) == 0)
+                {
+                        return i;
+                }
+        }
+
+        return -1;
+}
+
+char *
+logindb_cookieget(cearth_logindb *db, const char *user)
+{
+        int n = logindb_userget(db, user);
+        return db->cookie[n];
+}
+
+
+char *
+loginhttp_tokenget(const char *user, const char *cookie)
+{
+        printf("TOKENGET IS UNIMPLEMENTED. Args: user: %s, cookie: %s", user, cookie);
+        return("KEK!");
+}
+
+/* TODO store tokens in logindb as well. */
+char *
+logindb_tokenget(cearth_logindb *db, const char *user, const char *cookie)
+{
+        printf("TOKENGET IS UNIMPLEMENTED. Args: user: %s, cookie: %s", user, cookie);
+        return("KEK!");
 }
 
 void
-loginhttp_getcookie(const char *user)
+loginhttp_cookieget(cearth_logindb *db, const char *user)
 {
-        /* Obtain password from stdin
+        int check = logindb_usercheck(db, user); /* You can't trust outside data! */
+        switch(check) {
+                case 1:
+                        break;
+                case 0:
+                case 2:
+                default:
+                        return;
+        }
+        /*
+         * Obtain password from stdin
          * TODO Make password input hidden like in sudo/github login
          */
         printf("Password for user '%s': ", user);
         char password[24] = {0};
         fgets(password, 24, stdin);
+        /* Replace trailing newline with NULL */
         size_t nl = strlen(password) - 1;
         if (password[nl] == '\n')
                 password[nl] = '\0';
@@ -136,18 +182,42 @@ loginhttp_getcookie(const char *user)
         strcat(loginurl, "/portal/sec/login");
         curl_easy_setopt(handle, CURLOPT_URL, loginurl);
         curl_easy_setopt(handle, CURLOPT_POSTFIELDS, postdata);
+        curl_easy_setopt(handle, CURLOPT_COOKIEJAR, LOGIN_COOKIEJAR);
 
         curl_easy_perform(handle);
+        curl_easy_cleanup(handle);
 
         /* Read cookie if it exists. */
-        struct curl_slist *cookies = NULL;
-        curl_easy_getinfo(handle, CURLINFO_COOKIELIST, cookies);
-
-        if (cookies == NULL) {
-                fprintf(stderr, "loginhttp_getcookie: Failed to obtain cookies.\n");
-        } else {
-                printf("First cookie: %s", cookies->data);
+        FILE *fp = fopen(LOGIN_COOKIEJAR, "w+");
+        if (!fp) {
+                perror("cearth_login");
+                return;
+        }
+        char cookie[33];
+        while(!feof(fp)) {
+                char line[LOGINDB_MAXLINE];
+                fgets(line, LOGINDB_MAXLINE, fp);
+                sscanf(line, "www.havenandhearth.com\tFALSE\t/\tFALSE\t0\thsess\t%s", cookie);
         }
 
-        curl_easy_cleanup(handle);
+        fclose(fp);
+        remove(LOGIN_COOKIEJAR);
+
+        logindb_cookieset(db, user, cookie);
+}
+
+int
+logindb_cookieset(cearth_logindb *db, const char *user, const char *cookie)
+{
+        /* Invalid logindb */
+        if (db->n == 0)
+                return -1;
+        /* Invalid cookie  */
+        if (strlen(cookie) != LOGIN_COOKIESIZE)
+                return -1;
+
+        int n = logindb_userget(db, user);
+        strcpy(db->cookie[n], cookie);
+
+        return 0;
 }
--- src/net/cearth_login.h
+++ src/net/cearth_login.h
@@ -1,15 +1,15 @@
 #ifndef _LOGIN_H
 #define _LOGIN_H
 #include <stdio.h>
-#include <regex.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <SDL2/SDL_net.h>
 #include <curl/curl.h>
 
-#include "cearth_defines.h"
-#include "cearth_network.h"
-#include "cearth_config.h"
+#include "../config/cearth_defines.h"
+#include "../config/cearth_config.h"
+#include "../net/cearth_network.h"
 
 typedef struct
 {
@@ -20,34 +20,35 @@ typedef struct
 } cearth_logindb;
 
 /* Logs into havenandhearth.com and gets a session cookie */
-void loginhttp_cookieget(const char *user);
+void loginhttp_cookieget(cearth_logindb *, const char *user);
 /* gets /autohaven and gets a token in hexadecimal */
-void loginhttp_tokenget(const char *user, const char *cookie);
+char * loginhttp_tokenget(const char *user, const char *cookie);
 
-int logindb_useradd(cearth_logindb *, char *user);
-int logindb_userdel(cearth_logindb *, char *user);
+int logindb_useradd(cearth_logindb *, const char *user);
+int logindb_userdel(cearth_logindb *, const char *user);
 /*
  * Checks if a user exists in the logindb
  * Returns 0 if the user does not exists;
  * Returns 1 if the user exists but has no cookie.
  * Returns 2 if the user exists and has a cookie.
  */
-int logindb_usercheck(cearth_logindb *, char *user);
-int logindb_cookieget(cearth_logindb *, char *user);
-int logindb_cookieset(cearth_logindb *, char *user, char *cookie);
-int logindb_cookiedel(cearth_logindb *, char *user);
+int logindb_usercheck(cearth_logindb *, const char *user);
+int logindb_userget  (cearth_logindb *, const char *user);
+int logindb_cookieset(cearth_logindb *, const char *user, const char *cookie);
+int logindb_cookiedel(cearth_logindb *, const char *user);
+char * logindb_cookieget(cearth_logindb *, const char *user);
 /*
  * Requires user to have an assigned cookie or it will return NULL.
  * Retrieves hexadecimal cookie using logindb_tokenget
  * and returns a pointer to it.
  */
-char * logindb_tokenget(cearth_logindb *, char *user);
+char * logindb_tokenget(cearth_logindb *, const char *user, const char *cookie);
 
 /*
  * Opens LOGINDB_FILE and loads it into the appropriate struct.
  * @returns pointer to loaded logindb.
  */
-cearth_logindb *logindb_open(cearth_logindb *);
+cearth_logindb *logindb_open(void);
 /*
  * Saves updated logindb to LOGINDB_FILE and frees memory.
  * This must be called when cearth is done with logindb.
