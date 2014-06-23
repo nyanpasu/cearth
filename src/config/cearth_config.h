774128efb3a12ffbbedc6721defbcf45f4a317c7
nyanpasu <nyanpasugit@gmail.com>
2014-06-24 00:38:37 +0800

    Move cearth_config.h to correct dir
    cearth_config.h was in the wrong dir (src/utils)

---
+++ src/config/cearth_config.h
@@ -0,0 +1,13 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define LOGINDB_FILE "logindb"
+#define LOGINDB_MAX 100
+#define LOGINDB_MAXLINE 128
+
+static const char *haven_authserv    = "moltke.seatribe.se";
+static const char *haven_webauth     = "www.havenandhearth.com";
+static const char *cearth_config_dir = "/.cearth/";
+static const char *foo               = "test";
+
+#endif
