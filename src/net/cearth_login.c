1f9bd74869d138e22d7ef281ce72af59bcfaaf10
nyanpasu <nyanpasugit@gmail.com>
2014-06-25 02:13:17 +0800

    Fix for wrong type in loginhttp_tokenget

--- src/net/cearth_login.c
+++ src/net/cearth_login.c
@@ -154,7 +154,7 @@ loginhttp_tokenget(cearth_logindb *db, const char *user)
 
         /* Parse tmp file */
         fseek(buf, 0, SEEK_SET);
-        char *tok[LOGINDB_TOKENSIZE] = {0};
+        char tok[LOGINDB_TOKENSIZE] = {0};
         while(!feof(buf)) {
                 /* Finding the token string */
                 fscanf(buf, LOGIN_TOKSTRSTART"%s", tok);
