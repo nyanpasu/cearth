7a6ce14976db6989cbe27759d60e300ed6b00922
nyanpasu <nyanpasugit@gmail.com>
2014-06-25 00:30:59 +0800

    Add new #defines for maximum url string size and data for autohaven

--- src/config/cearth_config.h
+++ src/config/cearth_config.h
@@ -6,9 +6,11 @@
 #define LOGINDB_MAXLINE 128
 #define LOGIN_COOKIEJAR "cookie"
 #define LOGIN_COOKIESIZE 32
+#define LOGIN_URLMAXSIZE 256
 
 static const char *haven_authserv    = "moltke.seatribe.se";
 static const char *haven_webauth     = "www.havenandhearth.com";
+static const char *haven_tokenlink   = "/autohaven";
 static const char *cearth_config_dir = "/.cearth/";
 static const char *foo               = "test";
 
