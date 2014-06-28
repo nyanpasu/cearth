509975783e0a5933d34bc1073c5eaacb2d9a4ae6
nyanpasu <nyanpasugit@gmail.com>
2014-06-29 01:47:09 +0800

    Write implementation for cearth_gui

---
+++ src/ui/cearth_gui.h
@@ -0,0 +1,25 @@
+#ifndef _CEARTH_GUI
+#define _CEARTH_GUI
+/**
+ * FILE SUMMARY:
+ * Contains the struct definition for cearth_gui
+ * AND the modules cearth_gui can utilize to actually not
+ * be an useless struct.
+ * Use it with cearth_screen, which actually does the reading
+ * and rendering.
+ */
+
+typedef struct
+{
+} cearth_ui;
+
+/**
+ * Contains an array of pointers to various ui elements assigned to it.
+ * UNIMPLEMENTED
+ */
+typedef struct
+{
+        cearth_ui element[];
+} cearth_gui;
+
+#endif //#ifndef _CEARTH_GUI
