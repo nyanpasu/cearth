6522b065d1348d0cb2b9767f7128d78ce3e93b63
nyanpasu <nyanpasugit@gmail.com>
2014-06-29 02:14:44 +0800

    Write a main() function worth of 0.3.0
    Changes:
    - Changed whole workflow to reflect current vision of the program flow.
    - Left only function calls in main
    - Depreciated old login code.

--- src/cearth_main.c
+++ src/cearth_main.c
@@ -14,7 +14,11 @@
 #include "net/cearth_login.h"
 #include "net/cearth_ctx.h"
 
-void g_login(cearth_logindb * db, const char *user, char **cookie, char **token);
+void g_login     (cearth_logindb * db, const char *user, char **cookie, char **token);
+void g_getopt    (int, const char **);
+void g_sdlcreate (SDL_Window *, SDL_Renderer *);
+void g_sdldestroy(SDL_Window *, SDL_Renderer *);
+void g_init      ();
 
 int window_height = 600;
 int window_width = 800;
@@ -24,35 +28,13 @@ SDL_Window *g_window;
 
 int main(int argc, const char **argv)
 {
-        /* TODO Use getopt to handle parsing. */
-        if (argc > 1) {
-                if (strcmp(argv[1],"--version") + strcmp(argv[1],"-v")== 0) {
-                        utils_ver_print();
-                        return 0;
-                }
-        } else {
-                printf("Usage: %s <username>\n", argv[0]);
-                return 0;
-        }
+        g_getopt(argc, argv);
 
-        const char *arg_user = argv[1];
-
-        /* NOTE: Lines that are commented out are prototypes */
         /* Initialize libraries and set working dir to ~/.cearth */
-        if (utils_home_init() == 0)
-                exit(EXIT_FAILURE);
-        utils_lib_init();
+        g_init();
 
         /* Display SDL shit. */
-        g_window = SDL_CreateWindow("Cearth",
-                                     SDL_WINDOWPOS_CENTERED,
-                                     SDL_WINDOWPOS_CENTERED,
-                                     window_width,
-                                     window_height,
-                                     0);
-        g_renderer = SDL_CreateRenderer(g_window,
-                                         -1,
-                                         SDL_RENDERER_ACCELERATED);
+        g_sdlcreate(g_window, g_renderer);
 
         /* Load resources */
         cearth_resourcedb *resdb;
@@ -62,37 +44,91 @@ int main(int argc, const char **argv)
         cearth_screen *scr = cearthscreen_new();
         cearth_gui *gui = cearthgui_new();
 
-        cearthgui_bind(gui, ctx);
-
         cearthscreen_bindsdl(scr, g_renderer);
         cearthscreen_bindgui(scr, gui);
 
         g_login(gui);
 
+        /* Begin server context */
+        cearth_ctx *ctx = cearthctx_new();
+        cearthctx_guibind(ctx, gui);
+        cearthctx_setconn(ctx, arg_user, token);
+        cearthctx_startread(ctx);
+        cearthctx_startwrite(ctx);
+        cearthctx_startprcs(ctx);
+
         cearthscreen_start(scr);
+
         /* Character selection */
+        cearthgui_clear(gui);
+        g_charsel(gui);
+
         /* Game loop */
+        cearthgui_clear(gui);
+        cearthctx_guibind(ctx, gui);
+
         /* Disconnect, free memory and shutdown gracefully. */
         cearthctx_disconnect(ctx);
-        cearthgui_free(gui);
         cearthscreen_stop(scr);
+        cearthgui_clear(gui);
+        cearthgui_free(gui);
         cearthscreen_free(scr);
 
-        /* Login into website and obtain token. */
-        char *cookie, *token;
-        cearth_logindb *logdb = logindb_open();
-        g_login_old(logdb, arg_user, &cookie, &token);
+        /* DEPRECIATED */
+        // /* Login into website and obtain token. */
+        // char *cookie, *token;
+        // cearth_logindb *logdb = logindb_open();
+        // g_login_old(logdb, arg_user, &cookie, &token);
 
-        /* Attempt login into game servers */
-        cearth_ctx *ctx = cearthctx_new();
-        cearthctx_connect(ctx, arg_user, token);
+        // logindb_close(logdb);
 
-        logindb_close(logdb);
+        g_sdldestroy(g_window, g_renderer);
         utils_lib_deinit();
 
         return 0;
 }
 
+
+void
+g_sdlcreate(SDL_Window *w, SDL_Renderer *r)
+{
+        g_window = SDL_CreateWindow("Cearth",
+                                     SDL_WINDOWPOS_CENTERED,
+                                     SDL_WINDOWPOS_CENTERED,
+                                     window_width,
+                                     window_height,
+                                     0);
+        g_renderer = SDL_CreateRenderer(g_window,
+                                         -1,
+                                         SDL_RENDERER_ACCELERATED);
+}
+
+void
+g_sdldestroy(SDL_Window *w, SDL_Renderer *r)
+{
+        SDL_DestroyRenderer(r);
+        SDL_DestroyWindow(w);
+}
+
+void
+g_init()
+{
+        if (utils_home_init() == 0)
+                exit(EXIT_FAILURE);
+        utils_lib_init();
+}
+
+void
+g_getopt(int argc, const char **argv)
+{
+        /* TODO Use getopt to handle parsing. */
+        if (argc > 1) {
+                if (strcmp(argv[1],"--version") + strcmp(argv[1],"-v")== 0) {
+                        utils_ver_print();
+                        return 0;
+                }
+        }
+}
 
 void
 g_login_old(cearth_logindb *db, const char *user, char **cookie, char **token)
